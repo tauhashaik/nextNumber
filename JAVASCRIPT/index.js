6b7ba34e08496667305b327a203bf143797ceccb
tauhashaik <hoosains19@gmail.com>
2023-11-22 15:39:25 +0200

    added comments

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,10 +1,14 @@
-let arNum = [3, 5, 7, 9, 15,35 ];
+    // Create Array
+let arNum = [1, 3, 5, 7, 9, 11];
 let n = 0
+        // Creating Variables for button and display
         let nextInput = document.getElementById('nextInput');
         let calcbtn = document.getElementById('calcbtn');
 
+    // addEventListener for button functionality
         calcbtn.addEventListener('click', displayNextNumber)
-        
+
+        // function created to run through array until length variable reaches length and resets to 0.
         function displayNextNumber(){
             if(n < arNum.length){
                 let next = arNum[n];
